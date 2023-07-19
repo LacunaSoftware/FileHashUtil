2299bac73878ac970b9aed0a9ff6f96031ab7bce
Alexandre <alexswi@hotmail.com>
2023-07-19 20:34:41 -0300

    Progress info added

--- FileHash/Program.cs
+++ FileHash/Program.cs
@@ -6,6 +6,7 @@ using System.CommandLine;
 using System.Net.Mail;
 using System.Net;
 using DataSizeUnits;
+using System.Runtime.Intrinsics.Arm;
 
 namespace FileHash;
 internal class Program {
@@ -21,7 +22,7 @@ internal class Program {
 		rootCommand.AddOption(sha1Option);
 		rootCommand.AddOption(fileInfoOption);
 
-		rootCommand.SetHandler((file, sha256, sha1, fileInfo) => {
+		rootCommand.SetHandler((file, sha256opt, sha1opt, fileInfo) => {
 			Console.WriteLine("File Hash v1.1");
 			Console.WriteLine("");
 			if (!File.Exists(file)) {
@@ -33,23 +34,60 @@ internal class Program {
 				var size = string.Format(new DataSizeFormatter(), "{0:A1}", info.Length);
 				Console.WriteLine($"Name: {info.Name}");
 				if (fileInfo is null || fileInfo.Value) {
-					Console.WriteLine($"Size: {info.Length} bytes ({size})");
+					Console.WriteLine($"Size: {info.Length:N0} bytes ({size})");
 					Console.WriteLine($"Creation UTC date: {info.CreationTimeUtc:G}");
 					Console.WriteLine($"Last Write UTC date: {info.LastWriteTimeUtc:G}");
 				}
-
+				sha256opt ??= true;
+				sha1opt ??= false;
 				using FileStream fileStream = File.OpenRead(file);
-				using BufferedStream bufferedStream = new BufferedStream(fileStream, 1024 * 32);
+				var sha256 = SHA256.Create();
+				var sha1 = SHA1.Create();
+				var totalLength = fileStream.Length;
+				var bufferSize = 1024 * 1024;
+				var buffer = new byte[bufferSize];
+				int bytesRead;
+				long totalBytesRead = 0;
+				double percentage = 0;
 
-				if (sha256 is null || sha256.Value) {
-					var hashBytes = SHA256.Create().ComputeHash(bufferedStream);
-					var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
-					Console.WriteLine($"SHA256: {hash}");
+				while ((bytesRead = fileStream.Read(buffer, 0, bufferSize)) > 0) {
+					if (sha256opt.Value) {
+						sha256.TransformBlock(buffer, 0, bytesRead, null, 0);
+					}
+					if (sha1opt.Value) {
+						sha1.TransformBlock(buffer, 0, bytesRead, null, 0);
+					}
+					totalBytesRead += bytesRead;
+					percentage = (double)totalBytesRead / totalLength * 100;
+					PrintProgressBar(percentage);
 				}
-				if (sha1.HasValue && sha1.Value) {
-					var hashBytes = SHA1.Create().ComputeHash(bufferedStream);
-					var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
-					Console.WriteLine($"SHA1: {hash}");
+				if (sha256opt.Value) {
+					sha256.TransformFinalBlock(buffer, 0, 0);
+				}
+				if (sha1opt.Value) {
+					sha1.TransformFinalBlock(buffer, 0, 0);
+				}
+				if (sha256opt.Value) {
+					if (sha256.Hash is null) {
+						Console.Write("\r");
+						Console.WriteLine($"SHA256: ERROR");
+					} else {
+						byte[] finalHash = sha256.Hash;
+						string hash = BitConverter.ToString(finalHash).Replace("-", "").ToLowerInvariant();
+						Console.Write("\r"); // End of progress bar
+						Console.WriteLine($"SHA256: {hash}");
+					}
+				}
+				if (sha1opt.Value) {
+					if (sha1.Hash is null) {
+						Console.Write("\r");
+						Console.WriteLine($"SHA1: ERROR");
+					} else {
+						byte[] finalHash = sha1.Hash;
+						string hash = BitConverter.ToString(finalHash).Replace("-", "").ToLowerInvariant();
+						Console.Write("\r"); // End of progress bar
+						Console.WriteLine($"SHA1: {hash}");
+					}
 				}
 			} catch (Exception e) {
 				Console.WriteLine($"Error: {e.Message}");
@@ -58,6 +96,11 @@ internal class Program {
 		}, fileOption, sha256Option, sha1Option, fileInfoOption);
 		return await rootCommand.InvokeAsync(args);
 	}
+
+	static void PrintProgressBar(double percentage) {
+		Console.Write($"\r{percentage:N1}% Completed");
+	}
+
 }
 
 
